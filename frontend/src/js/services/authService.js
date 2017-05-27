25b3fdfb22f89fccbc403b4dc3fe820da2bc8a5d
Marcin Grygierek <grygierek.marcin@gmail.com>
2017-05-27 18:59:04 +0200

    updated authService to use localStorage instead of sessionStorage

--- frontend/src/js/services/authService.js
+++ frontend/src/js/services/authService.js
@@ -1,20 +1,20 @@
 export default class AuthService {
     providerLogin(email, password) {
         if(email === 'provider@email.com' && password === 'provider') {
-            sessionStorage.setItem('token', 123456);
+            localStorage.setItem('token', 123456);
             return true;
         }
         return false;
     }
 
     logout() {
-        sessionStorage.clear();
-        console.log(sessionStorage.getItem('token'));
+        localStorage.clear();
+        console.log(localStorage.getItem('token'));
         return true;
     }
 
     isSignedIn() {
-        const token = sessionStorage.getItem('token');
+        const token = localStorage.getItem('token');
         console.log(token ? true : false);
         return token ? true : false;
     }
